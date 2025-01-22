f2db2767d4677450180e9d127ecb67cca0e22c2d
rostin16 <roshnijp16@gmail.com>
2025-01-22 23:36:23 +0530

    new

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const port=8081;
+const bodyParser=require('body-parser');
+const db= require('./config/database');
+
+app.set('view engine', 'ejs');
+app.use(express.static('assets/client'));
+app.use(express.static('assets/admin'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname + '/uploads')))
+app.use('/', require('./routers'));
+
+app.listen(port,(err) => {
+    if (!err) {
+        db(); //this is for connect to database.when error not occure
+        console.log("server start successfully...");
+        console.log("http://localhost:" +port);
+        
+        
+    }
+})
---
+++ models/movieModels.js
@@ -0,0 +1,15 @@
+const { default: mongoose } = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    image: String,
+    rating: String,
+},{ 
+    timestamps: true,
+
+})
+const movieModle = mongoose.model('movieModel', movieSchema);
+module.exports=movieModle;
